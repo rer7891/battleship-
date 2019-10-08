df6803689d247417c8bcf2fe74f92f38f5349860
Becky Robran <rerobran@gmail.com>
2019-10-07 19:48:42 -0600

    Added hit and sunk functionality

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,7 +1,17 @@
 class Ship
-  attr_reader :name, :length
+  attr_reader :name, :length, :health, :sunk_status
   def initialize(name, length)
     @name = name
     @length = length
+    @health = length
+  end
+  def hit
+   @health -= 1
+  end
+  def sunk?
+    if @health >= 1
+      @sunk_status = false
+    else
+      @sunk_status = true
   end 
 end
