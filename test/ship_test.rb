6c9395fb6dae32385f6072adf37aad04b4e5e37a
Becky Robran <rerobran@gmail.com>
2019-10-07 19:37:25 -0600

    added exists,name,length tests

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -2,4 +2,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib.ship.rb'
 
-class ShipTest < Minitest::Test 
+class ShipTest < Minitest::Test
+
+  def test_it_exists
+    cruiser = Ship.new("Cruiser", 3)
+    assert_instance_of Ship, cruiser
+  end
+  def test_it_has_a_name
+  end
+  def test_it_has_a_length
+  end
+end
