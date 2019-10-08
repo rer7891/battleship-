a958b02c832bc61726ae72386d6859db93cb6759
Becky Robran <rerobran@gmail.com>
2019-10-07 19:40:20 -0600

    added functionality to tests

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -3,13 +3,16 @@ require 'minitest/pride'
 require './lib.ship.rb'
 
 class ShipTest < Minitest::Test
-
-  def test_it_exists
+  def setup
     cruiser = Ship.new("Cruiser", 3)
+  end
+  def test_it_exists
     assert_instance_of Ship, cruiser
   end
   def test_it_has_a_name
+    assert_equal "Cruiser", cruiser.name
   end
   def test_it_has_a_length
+    assert_equal 3, cruiser.length 
   end
 end
