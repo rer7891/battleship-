3c473cc86aa53f5709228965a0c2f1066f5bdb4c
Becky Robran <rerobran@gmail.com>
2019-10-07 19:42:51 -0600

    Fixed setup with instance variable

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -1,18 +1,18 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib.ship.rb'
+require './lib/ship'
 
 class ShipTest < Minitest::Test
   def setup
-    cruiser = Ship.new("Cruiser", 3)
+    @cruiser = Ship.new("Cruiser", 3)
   end
   def test_it_exists
-    assert_instance_of Ship, cruiser
+    assert_instance_of Ship, @cruiser
   end
   def test_it_has_a_name
-    assert_equal "Cruiser", cruiser.name
+    assert_equal "Cruiser", @cruiser.name
   end
   def test_it_has_a_length
-    assert_equal 3, cruiser.length 
+    assert_equal 3, @cruiser.length
   end
 end
