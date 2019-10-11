d2e1d148fd7b599db8d427ef85b51658f06bb12f
Becky Robran <rerobran@gmail.com>
2019-10-11 11:43:03 -0600

    "Adds place ship tests"
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>

--- test/board_test.rb
+++ test/board_test.rb
@@ -9,6 +9,9 @@ class BoardTest < Minitest::Test
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
+    @cell_1 = @board.cells["A1"]
+    @cell_2 = @board.cells["A2"]
+    @cell_3 = @board.cells["A3"]
   end
 
   def test_it_exists
@@ -48,5 +51,12 @@ class BoardTest < Minitest::Test
     assert_equal false, @board.validate_placement?(@submarine, ["C2", "D3"])
     assert_equal true, @board.validate_placement?(@submarine, ["A1", "A2"])
     assert_equal true, @board.validate_placement?(@cruiser, ["B1", "C1", "D1"])
+  end
+  def test_it_can_place_a_ship
+    @board.place(@cruiser, ["A1", "A2", "A3"])
+    assert_equal @cruiser, @cell_1.ship
+    assert_equal @cruiser, @cell_2.ship
+    assert_equal @cruiser, @cell_3.ship
+    assert @cell_3.ship == @cell_2.ship 
   end
 end
