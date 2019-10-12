d2a056806cd7c68549eecddd53530a8ffb40bd20
Becky Robran <rerobran@gmail.com>
2019-10-11 20:41:37 -0600

    "Add v1 of ship not overlapping"

--- lib/board.rb
+++ lib/board.rb
@@ -6,6 +6,7 @@ class Board
               "B1" => Cell.new("B1"), "B2" => Cell.new("B2"), "B3" => Cell.new("B3"), "B4" => Cell.new("B4"),
               "C1" => Cell.new("C1"), "C2" => Cell.new("C2"), "C3" => Cell.new("C3"), "C4" => Cell.new("C4"),
               "D1" => Cell.new("D1"), "D2" => Cell.new("D2"), "D3" => Cell.new("D3"), "D4" => Cell.new("D4")}
+    @used_coords = []
   end
 
   def valid_coordinate?(coors)
@@ -13,15 +14,16 @@ class Board
   end
 
   def validate_placement?(ship, coors)
-    some = coors.map {|coor| valid_coordinate?(coors)}
-    some.all? {|coor| coor == coors}
+    return false if !coors.all? {|coor| valid_coordinate?(coor) }# || ship_placement(coors)
 
-    verify_length(ship, coors) && verify_horizontal(ship, coors)|| verify_vertical(coors)
+    verify_length(ship, coors) && verify_horizontal(ship, coors) || verify_vertical(coors)
 
   end
+
   def verify_length(ship, coors)
     coors.length == ship.length
   end
+
   def verify_horizontal(ship, coors)
 
     horizonal_character = coors.map { |x| x[0] }
@@ -29,18 +31,37 @@ class Board
 
     (horizonal_character.all? { |char| char == horizonal_character.first}) && (vertical_character.each_cons(2).all? {|a, b| b == a + 1})
   end
+
   def verify_vertical(coors)
 
     vertical_letter = coors.map {|x| x[0].ord}
 
     vertical_number = coors.map { |x| x[1].to_i }
 
-      (vertical_number.all? { |num| num == vertical_number.first}) && (vertical_letter.each_cons(2).all? {|a, b| b == a + 1})
+    (vertical_number.all? { |num| num == vertical_number.first}) && (vertical_letter.each_cons(2).all? {|a, b| b == a + 1})
+  end
+
+  def used_coordinates(coors)
+    @used_coords = coors.map {|coor| coor }
+  end
+
+  def ship_placement(coors)
+    num = -1
+     coors.any? do |coor|
+       num += 1
+       coor == @used_coords[num]
+     end
   end
 
   def place(ship, coors)
-    coors.map do |key, value|
-      cell.place_ship(ship) if key == coors
+
+    used_coordinates(coors)
+
+    coors.map do |coor|
+      coor == @cells[coor]
+      @cells[coor].place_ship(ship)
     end
   end
+
+
 end
