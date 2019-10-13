a412eda7d8211099cbf57b673cbc712473888a21
Becky Robran <rerobran@gmail.com>
2019-10-13 13:27:25 -0600

    Fixes render ship in cell

--- lib/cell.rb
+++ lib/cell.rb
@@ -29,16 +29,16 @@ class Cell
   end
 
   def render(ship_view = false)
-    if fired_upon? == false && ship_view == false
-      "."
-    elsif ship_view = true && fired_upon? == false
+    if ship_view = true && fired_upon? == false && !empty?
       "S"
-    elsif empty? && fired_upon? == true
+    elsif fired_upon? == false && ship_view == false
+      "."
+    elsif empty? && fired_upon? == false
       "M"
-    elsif ship.sunk?
-      "X"
     elsif !empty? && fired_upon? == true
       "H"
+    elsif ship.sunk?
+      "X"
     end
   end
 end
