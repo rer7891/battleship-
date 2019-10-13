1cc590ca8ce44db4329e6f47438b813fee64c675
Becky Robran <rerobran@gmail.com>
2019-10-13 13:27:49 -0600

    Adds runner functionality

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,12 +1,19 @@
-# require './lib/ship'
-# require './lib/cell'
-# @cell = Cell.new("B4")
-# @cruiser = Ship.new("Cruiser", 3)
-# @board = Board.new
-#
-# @board.validate_placement(@cruiser, ["A1", "A2", "A3"])
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/play_game'
+
+@cell = Cell.new("B4")
+@cruiser = Ship.new("Cruiser", 3)
+@board = Board.new
+@play = PlayBattleShip(@board)
+
+@board.place(@cruiser, ["A1","A2","A3"])
+@board.render(true)
+@play.start_game
 
 
-(A..D).each {|letter| letter + 1)
-  # (1..4).each do {|number| number + }
-  end
+
+# (A..D).each {|letter| letter + 1)
+#   # (1..4).each do {|number| number + }
+# end
