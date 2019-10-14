841e60dcf44b8e081197a462f72e4ba05322bf75
Becky Robran <rerobran@gmail.com>
2019-10-14 08:36:45 -0600

    "Fixes place_computer ship"

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -15,31 +15,37 @@ class BattleShip
         puts "Goodbye from Battleship. Play again soon."
       end
   end
-  def place_computer_ships(ship)
+  def place_computer_ship(ship)
 
-    coords = @board.cells.values.sample(2)
-    new_coords = coords.map do |coord|
-     coord.coordinate
-    end
+    coords = @board.cells.values.sample(ship.length)
+      new_coords = coords.map do |coord|
+        coord.coordinate
+      end
 
-    while @board.ship_placed?(new_coords) == false
+    valid_coordinates = false
+
+    while valid_coordinates == false
       if @board.validate_placement?(ship, new_coords) == true
         @board.place(ship, new_coords)
+        valid_coordinates = true
       else
-        coords = @board.cells.values.sample(2)
+        coords = @board.cells.values.sample(ship.length)
           new_coords = coords.map do |coord|
           coord.coordinate
         end
       end
     end
   end
-  def place_player_ships
-  end 
+
+  def place_player_ship
+  end
+
   def setup_game
     @ship_1 = Ship.new("submarine", 2)
     @ship_2 = Ship.new("cruiser", 3)
-    place_game_ships(@ship_1)
-    place_game_ships(@ship_2)
+    place_computer_ship(@ship_1)
+    place_computer_ship(@ship_2)
+    @board.render
   end
 
   def take_turn
