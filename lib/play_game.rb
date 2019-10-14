9f4b6fd3bc948f55ab4db304ea2fc76e57b4a899
Becky Robran <rerobran@gmail.com>
2019-10-14 07:47:36 -0600

    "Adds game start and place ship"

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -1,20 +1,45 @@
-class PlayBattleShip
+class BattleShip
+  attr_reader :ship
+
   def initialize(board)
     @board = board
   end
-  def start_game
-   puts "Welcome to Battleship.
-   puts  Enter p to Play or q to Quit"
-   input = gets.chomp.downcase
-     if input ==  "p"
-       setup_game
-    else
-       puts "Goodbye from Battleship. Play again soon."
-     end
-  end
 
+  def start_game
+    puts "Welcome to Battleship.
+    puts  Enter p to Play or q to Quit"
+    input = gets.chomp.downcase
+      if input ==  "p"
+        setup_game
+      else
+        puts "Goodbye from Battleship. Play again soon."
+      end
+  end
+  def place_computer_ships(ship)
+
+    coords = @board.cells.values.sample(2)
+    new_coords = coords.map do |coord|
+     coord.coordinate
+    end
+
+    while @board.ship_placed?(new_coords) == false
+      if @board.validate_placement?(ship, new_coords) == true
+        @board.place(ship, new_coords)
+      else
+        coords = @board.cells.values.sample(2)
+          new_coords = coords.map do |coord|
+          coord.coordinate
+        end
+      end
+    end
+  end
+  def place_player_ships
+  end 
   def setup_game
-   p @board.render
+    @ship_1 = Ship.new("submarine", 2)
+    @ship_2 = Ship.new("cruiser", 3)
+    place_game_ships(@ship_1)
+    place_game_ships(@ship_2)
   end
 
   def take_turn
