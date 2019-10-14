70bd767650da2e3bc7517f8fd9e5be355d12388d
Becky Robran <rerobran@gmail.com>
2019-10-14 08:36:16 -0600

    "Fixes spacing"

--- lib/board.rb
+++ lib/board.rb
@@ -48,6 +48,7 @@ class Board
     end
   end
   def render(ship_view = false)
+  
     if ship_view == false
       puts  "  1 2 3 4 \n "
       puts   "A #{@cells["A1"].render} #{@cells["A2"].render} #{@cells["A3"].render} #{@cells["A4"].render} \n"
