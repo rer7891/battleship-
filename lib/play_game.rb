82a5158c9c11295b681b90c19417c6c8fcd4f31f
Becky Robran <rerobran@gmail.com>
2019-10-15 13:24:38 -0600

    "Fixes place user ship"

--- lib/play_game.rb
+++ lib/play_game.rb
@@ -1,14 +1,38 @@
 class BattleShip
   attr_reader :ship
 
-  def initialize(board)
-    @board = board
+  def initialize
+    @computer_board = Board.new
+    @player_board = Board.new
     @computer_ship_1 = Ship.new("submarine", 2)
     @computer_ship_2 = Ship.new("cruiser", 3)
     @user_ship_1 = Ship.new("cruiser", 3)
     @user_ship_2 = Ship.new("submarine", 2)
   end
 
+  def place_computer_ship(ship)
+
+    coords = @computer_board.cells.keys.sample(ship.length)
+
+    until @computer_board.validate_placement?(ship, coords)
+        coords = @computer_board.cells.keys.sample(ship.length)
+    end
+      @computer_board.place(ship, coords)
+  end
+
+  def place_player_ship(ship)
+    loop do
+      puts "Enter the squares for the #{ship.name.capitalize}(#{ship.length} spaces):"
+      @user_placement = gets.chomp.split(" ")
+      @user_placement
+        if @player_board.validate_placement?(ship, @user_placement)
+           @player_board.place(ship, @user_placement)
+           break
+        else
+          puts "Those are invalid coordinates. Please try again."
+        end
+    end
+  end
   def start_game
     puts "Welcome to Battleship."
     puts  "Enter p to Play or q to Quit"
@@ -18,32 +42,6 @@ class BattleShip
       else
         puts "Goodbye from Battleship. Play again soon."
       end
-  end
-  def place_computer_ship(ship)
-
-    coords = @board.cells.keys.sample(ship.length)
-
-    until @board.validate_placement?(ship, coords)
-        coords = @board.cells.keys.sample(ship.length)
-    end
-      @board.place(ship, coords)
-  end
-
-  def place_player_ship(ship)
-
-    puts "Enter the squares for the #{ship.name.capitalize}(#{ship.length} spaces):"
-    user_placement = gets.chomp.split(" ")
-
-     until @board.ship_placed?(user_placement)
-      if @board.validate_placement?(ship, user_placement)
-        @board.place(ship, user_placement)
-      else
-          # "Those are invalid coordinates. Please try again."
-          # puts "Enter the squares for the #{ship.name.capitalize}(#{ship.length} spaces):"
-          # user_placement = gets.chomp.split(" ")
-        puts "X"
-      end
-    end
   end
 
   def computer_setup_game
@@ -59,11 +57,14 @@ class BattleShip
     puts "You now need to lay out your two ships."
     puts "The Cruiser is three units long and the Submarine is two units long"
 
-    @board.render
+    @player_board.render
 
     place_player_ship(@user_ship_1)
     place_player_ship(@user_ship_2)
+
+    @player_board.render(true)
   end
+
 
   def take_turn
     user_placement
